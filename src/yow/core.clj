45dab55d1bcae6b3a482be210b3389616c97be0b
Aaron Bedra <aaron@aaronbedra.com>
2012-11-20 08:43:09 -0600

    Added initial exploration
    I'm building this from scratch and using my org-mode outline to store
    the steps as I do it. This will allow me to track the steps as I go and
    the slides should pretty much build themselves.
    
    The initial idea of producing the commands to Redis and sending data
    across the wire is complete. Next up is processing the data that is
    returned from Redis. This should be preceeded by an introduction to
    Polymorphism in Clojure.
    
    Signed-off-by: Aaron Bedra <aaron@aaronbedra.com>

--- src/yow/core.clj
+++ src/yow/core.clj
@@ -1,6 +1,32 @@
-(ns yow.core)
+(ns yow.core
+  (:require [clojure.string :as str])
+  (:import (java.net Socket)
+           (java.io BufferedInputStream DataInputStream)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def crlf "\r\n")
+
+(defn command
+  "Constructs a complete command string to send to Redis.
+   Conforms to the Redis unified protocol"
+  [name & args]
+  (str "*"
+       (inc (count args)) crlf
+       "$" (count name) crlf
+       (str/upper-case name) crlf
+       (str/join crlf
+                 (map (fn [x] (str "$" (count x) crlf x)) args))
+       crlf))
+
+(defn- socket
+  []
+  (doto (Socket. "localhost" 6379)
+    (.setTcpNoDelay true)
+    (.setKeepAlive true)))
+
+(defn request
+  [query]
+  (with-open [socket (socket)
+              in (DataInputStream. (BufferedInputStream. (.getInputStream socket)))
+              out (.getOutputStream socket)]
+    (.write out (.getBytes (apply str query)))
+    (println in)))
--- test/yow/core_test.clj
+++ test/yow/core_test.clj
@@ -2,6 +2,7 @@
   (:use clojure.test
         yow.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-command
+  (testing "Produces a well formed command string"
+    (is (= "*3\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$7\r\nmyvalue\r\n"
+           (command "set" "mykey" "myvalue")))))
